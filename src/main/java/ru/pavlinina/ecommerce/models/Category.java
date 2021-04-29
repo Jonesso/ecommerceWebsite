cb6b0d438f06c9d99fd4f512555c5faee036ed41
Sophie P <sonya.pav25@gmail.com>
2021-04-29 22:22:26 +0300

    Fix: manager category view

--- src/main/java/ru/pavlinina/ecommerce/models/Category.java
+++ src/main/java/ru/pavlinina/ecommerce/models/Category.java
@@ -1,5 +1,8 @@
 package ru.pavlinina.ecommerce.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +12,8 @@ import java.util.List;
  */
 @Entity
 @Table(name = "category")
+@Getter
+@Setter
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
