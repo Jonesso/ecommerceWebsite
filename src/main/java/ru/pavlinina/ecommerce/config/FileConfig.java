fef720369e73524f86b808c4b8e88b935ef9cf23
Sophie P <sonya.pav25@gmail.com>
2021-05-02 19:08:55 +0300

    Fix: bug in file config

--- src/main/java/ru/pavlinina/ecommerce/config/FileConfig.java
+++ src/main/java/ru/pavlinina/ecommerce/config/FileConfig.java
@@ -1,11 +1,13 @@
 package ru.pavlinina.ecommerce.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author Sofia Pavlinina
  */
+@Configuration
 public class FileConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
